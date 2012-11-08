11142e9807b209316a157ee52986be5407bcff8b
Julian Mueller <julmuell@homer.in.htwg-konstanz.de>
2012-11-08 10:40:30 +0100

    added php bla

---
+++ seapal/index.php
@@ -0,0 +1,72 @@
+<?php
+
+include("header.php");
+
+?>
+
+<div class="container">
+
+		<div class="hero-unit">
+			<div class="hidden-phone hidden-tablet">
+				<img src="http://placehold.it/960x150">
+			</div>
+			<div class="hidden-desktop hidden-phone">
+				<img src="http://placehold.it/724x150">
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="span4"><div class="round">
+				<h2>Bla</h2>
+				<p>sdfsdf sdf sd fs df sd f sdf</p></div></div>
+				<div class="span4">
+					<div class="round">
+						Span 4
+					</div>
+				</div>
+				<div class="span4"><div class="round">Span 4</div></div>
+			</div>
+
+			<div class="row">
+				<div class="span6"><div class="round">
+					<h2>Extensions</h2></div></div>
+				<div class="span6"><div class="round">Span 4</div></div>
+			</div>
+
+			<div class="row">
+				<div class="span12"><div class="round"><h2>dfgdf</h2>
+					<div class="entry">
+						<p>
+							<img src="http://placehold.it/724x150">
+						</p>
+						<p>
+							<b>SeaPal</b> is an App for sailors and other water sport fanatics that support the skipper in his tasks for navigation, route planing and logbook keeping. The central view shows a map with the current position and forward line. Waypoint and routes can easily be created, distances and courses can quickly be determined and documented in the logbook. The touch interface allows for fast and intuitive interaction.
+						</p>
+						<p>
+							Online maps are available from Google and OpenSeaMap, allong with satellite view. With these a trip can easily be planed and documented. In navigation the offline operation and the precision and depth of detail is of special importance. Thats why digital sea maps for selected areas can be downloaded and used offline.
+						</p>
+						<p>
+							For the area of the Lake Constance excellent sea maps and local information (harbours, shipyards, shops, restaurants) provided by the IBN publishing house are available as extensions. Further areas are in preparation.
+						</p>
+						<p>
+							The application was developed particularly for the iPad in a cooperation between the IBN publishing house and the HTWG Constance University. The specific advantages of a mobile device and the touch interface were taken into account just as well as the special requirements of sailors and skippers. As a result of this cooperation, <b>SeaPal</b> is made available for free. Extensions and sea maps can be purchased and loaded according to your area and needs.
+						</p>
+						<p>
+							This cooperation is open to further extension. Should you be interested to develop or provide further extensions, please contact us.
+						</p>
+						<p>
+							The application runs on the iPad and with reduced functionality on the iPhone. An iPad with 3G or 4G and GPS is recommended. For longer use an external power source (like a USP port of a radio) is required.
+						</p>
+					</div></div></div>
+				</div>
+
+			</div>
+		</div>
+		
+	</div>
+
+<?php
+
+include("footer.php");
+
+?>
