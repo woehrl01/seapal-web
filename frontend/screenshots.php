28eed179a1a196a5433aef9c1e9f2bbc995af1ab
Lukas Woehrl <lukas@woehrl.net>
2012-12-19 20:22:12 +0100

    Added some screenshots

--- frontend/screenshots.php
+++ frontend/screenshots.php
@@ -11,7 +11,111 @@
 		</div>
 
 		<div class="content-wrapper">
-			
+			<div class="container">
+				<div class="row">
+					<div class="span2">
+					</div>
+					<div class="span8">
+					<h1>SeaPal on the iPad</h1>
+					<div id="slideshow" class="carousel slide">
+					  <div class="carousel-inner">
+					    <div class="active item">
+					    	<img src="screenshots/SeaPal-initialPosition.jpg" alt="" />
+					    	<div class="carousel-caption">
+					    		<p>
+					    			Map view with current position, initial view
+					    		</p>
+					    	</div>
+					    </div>
+					    <div class="item">
+					    	<img src="screenshots/SeaPal-Gothenborg-Menu.jpg" alt="" />
+					    	<div class="carousel-caption">
+					    		<p>
+					    			Menu selected
+					    		</p>
+					    	</div>
+					    </div>
+					    <div class="item">
+					    	<img src="screenshots/SeaPal-Gothenborg-Tools.jpg" alt="" />
+					    	<div class="carousel-caption">
+					    		<p>
+					    			Tools selected
+					    		</p>
+					    	</div>
+					    </div>
+					    <div class="item">
+					    	<img src="screenshots/SeaPal-Gothenborg-OSM.jpg" alt="" />
+					    	<div class="carousel-caption">
+					    		<p>
+					    			Map view with OpenSeaMap showing Gothenburg
+					    		</p>
+					    	</div>
+					    </div>
+					    <div class="item">
+					    	<img src="screenshots/SeaPal-SatelliteView.jpg" alt="" />
+					    	<div class="carousel-caption">
+					    		<p>
+					    			Satellite view
+					    		</p>
+					    	</div>
+					    </div>
+					    <div class="item">
+					    	<img src="screenshots/SeaPal-Satellite-Closeup.jpg" alt="" />
+					    	<div class="carousel-caption">
+					    		<p>
+					    			Satellite view - Close-up
+					    		</p>
+					    	</div>
+					    </div>
+					    <div class="item">
+					    	<img src="screenshots/SeaPal-Southampton-OSM.jpg" alt="" />
+					    	<div class="carousel-caption">
+					    		<p>
+					    			Map view with OpenSeaMap showing Southampton
+					    		</p>
+					    	</div>
+					    </div>
+					    <div class="item">
+					    	<img src="screenshots/SeaPal-Southampton-OSM.jpg" alt="" />
+					    	<div class="carousel-caption">
+					    		<p>
+					    			Map view with OpenSeaMap showing Southampton
+					    		</p>
+					    	</div>
+					    </div>
+					    <div class="item">
+					    	<img src="screenshots/SeaPal-Konstanz-IBN.jpg" alt="" />
+					    	<div class="carousel-caption">
+					    		<p>
+					    			Offline map from IBN
+					    		</p>
+					    	</div>
+					    </div>
+					    <div class="item">
+					    	<img src="screenshots/SeaPal-POI-Search.jpg" alt="" />
+					    	<div class="carousel-caption">
+					    		<p>
+					    			Searching for Points of Interest
+					    		</p>
+					    	</div>
+					    </div>
+					    <div class="item">
+					    	<img src="screenshots/SeaPal-POI-Harborinfo.jpg" alt="" />
+					    	<div class="carousel-caption">
+					    		<p>
+					    			Points of Interest: Info for a Harbour
+					    		</p>
+					    	</div>
+					    </div>
+					  </div>
+					  <!-- Carousel nav -->
+					  <a class="carousel-control left" href="#slideshow" data-slide="prev">&lsaquo;</a>
+					  <a class="carousel-control right" href="#slideshow" data-slide="next">&rsaquo;</a>
+					</div>
+					<div class="span2">
+					</div>
+				</div>
+			</div>
 		</div>
 
 		<div class="footer-wrapper">
