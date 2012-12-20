83b20bdb1860e35a3c1dab3946b6a92978812c8a
Lukas Woehrl <lukas@woehrl.net>
2012-12-20 16:51:39 +0100

    Optimized look of footer

--- frontend/footer.php
+++ frontend/footer.php
@@ -14,16 +14,17 @@
 			<div class="span4">
 				<div class="round">
 					<h2>Help us promote SeaPal</h2>
-					<div style="height: 131px; width: 100%; display: inline-block; text-indent: 0px; margin: 0px; padding: 0px; background-color: transparent; border-style: none; float: none; line-height: normal; font-size: 1px; vertical-align: baseline; background-position: initial initial; background-repeat: initial initial;" id="___plus_0"><iframe frameborder="0" hspace="0" marginheight="0" marginwidth="0" scrolling="no" style="position: static; top: 0px; width: 300px; margin: 0px; border-style: none; left: 0px; visibility: visible; height: 131px;" tabindex="0" vspace="0" width="100%" id="I0_1356007279721" name="I0_1356007279721" src="https://plusone.google.com/u/0/_/pages/badge?bsv&amp;rel=publisher&amp;hl=en-US&amp;origin=http%3A%2F%2Fseapal.info&amp;url=http%3A%2F%2Fplus.google.com%2F100825752167882077539&amp;ic=1&amp;jsh=m%3B%2F_%2Fscs%2Fapps-static%2F_%2Fjs%2Fk%3Doz.gapi.de.t2rPVf5dnj4.O%2Fm%3D__features__%2Fam%3DiQ%2Frt%3Dj%2Fd%3D1%2Frs%3DAItRSTPBnQfeZQOCeLuYQaAXWexMCxmU5g#_methods=onPlusOne%2C_ready%2C_close%2C_open%2C_resizeMe%2C_renderstart%2Concircled%2Conauth%2Conload&amp;id=I0_1356007279721&amp;parent=http%3A%2F%2Fseapal.info" allowtransparency="true" title="Google+ Badge"></iframe></div>
-				
-					<script type="text/javascript">
-					  (function() {
-						var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-						po.src = 'https://apis.google.com/js/plusone.js';
-						var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-					  })();
-					</script>
-					<div class="fb-like fb_edge_widget_with_comment fb_iframe_widget" data-href="http://www.SeaPal.info" data-send="true" data-width="300" data-show-faces="true" fb-xfbml-state="rendered"><span style="height: 28px; width: 300px;"><iframe id="fd716d2bc" name="f39c39721" scrolling="no" style="border: none; overflow: hidden; height: 28px; width: 300px;" title="Like this content on Facebook." class="fb_ltr" src="http://www.facebook.com/plugins/like.php?api_key=&amp;locale=en_US&amp;sdk=joey&amp;channel_url=http%3A%2F%2Fstatic.ak.facebook.com%2Fconnect%2Fxd_arbiter.php%3Fversion%3D17%23cb%3Df5a1b2e8%26origin%3Dhttp%253A%252F%252Fseapal.info%252Ff3ad33c32%26domain%3Dseapal.info%26relation%3Dparent.parent&amp;href=http%3A%2F%2Fwww.SeaPal.info&amp;node_type=link&amp;width=300&amp;layout=standard&amp;colorscheme=light&amp;show_faces=true&amp;send=true&amp;extended_social_context=false"></iframe></span></div>
+					<div class="row">
+						<div class="span1">
+							<!-- Google + -->
+							<a href="https://plus.google.com/100825752167882077539?prsrc=3" rel="publisher" style="text-decoration:none;">
+							<img src="//ssl.gstatic.com/images/icons/gplus-64.png" alt="Google+" style="border:0;width:64px;height:64px;"/></a>
+						</div>
+						<div class="span2">
+							<!-- Facebook -->
+							<iframe src="http://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.seapal.info&amp;layout=box_count&amp;show_faces=true&amp;width=450&amp;action=like&amp;colorscheme=light&amp;height=80" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100%; height:80px;" allowTransparency="true"></iframe>
+						</div>
+					</div>
 				</div>
 			</div>
 			<div class="span4">
