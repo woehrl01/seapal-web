b5891e7d1ba264f796bc5b83edc5c24f9a91dd69
Lukas Woehrl <lukas@woehrl.net>
2013-05-10 22:09:22 +0200

    Added lisening on database changes

--- app/de/htwg/seapal/module/SeapalBaseModule.java
+++ app/de/htwg/seapal/module/SeapalBaseModule.java
@@ -1,6 +1,15 @@
 package de.htwg.seapal.module;
 
+import org.ektorp.CouchDbInstance;
+import org.ektorp.http.HttpClient;
+import org.ektorp.http.StdHttpClient;
+import org.ektorp.impl.StdCouchDbInstance;
+
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import com.google.inject.name.Named;
+import com.google.inject.name.Names;
 
 import de.htwg.seapal.controller.IBoatController;
 import de.htwg.seapal.controller.ITripController;
@@ -16,9 +25,22 @@ public abstract class SeapalBaseModule extends AbstractModule {
 	@Override
 	protected void configure() {
 		bind(ILogger.class).to(WebLogger.class);
-		bind(IBoatController.class).to(BoatController.class);
-		bind(ITripController.class).to(TripController.class);
-		bind(IWaypointController.class).to(WaypointController.class);
+		bind(IBoatController.class).to(BoatController.class).in(Singleton.class);
+		bind(ITripController.class).to(TripController.class).in(Singleton.class);
+		bind(IWaypointController.class).to(WaypointController.class).in(Singleton.class);
+		
+		bind(String.class).annotatedWith(Names.named("databaseHost")).toInstance("roroettg.iriscouch.com");
+	    bind(Integer.class).annotatedWith(Names.named("databasePort")).toInstance(80);
 	}
+	
+	@Provides
+    HttpClient getHttpClient(@Named("databaseHost") String databaseHost, @Named("databasePort") int databasePort) {
+        return new StdHttpClient.Builder().host(databaseHost).port(databasePort).build();
+    }
+
+    @Provides
+    CouchDbInstance getStdCouchDbInstance(HttpClient httpClient) {
+        return new StdCouchDbInstance(httpClient);
+    }
 
 }
--- app/de/htwg/seapal/web/controllers/BoatPositionAPI.java
+++ app/de/htwg/seapal/web/controllers/BoatPositionAPI.java
@@ -2,8 +2,15 @@ package de.htwg.seapal.web.controllers;
 
 import java.util.concurrent.Callable;
 
-import de.htwg.seapal.web.models.BoatPosition;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.node.JsonNodeFactory;
+import org.ektorp.CouchDbConnector;
+import org.ektorp.CouchDbInstance;
+import org.ektorp.changes.ChangesCommand;
+import org.ektorp.changes.ChangesFeed;
+import org.ektorp.changes.DocumentChange;
 
+import com.google.inject.Inject;
 
 import static play.libs.Akka.future;
 import play.libs.F.*;
@@ -14,19 +21,34 @@ import play.mvc.Result;
 
 public class BoatPositionAPI extends Controller {
 	
-	private static BoatPosition position = new BoatPosition(47.655, 9.21006);
+	@Inject
+	private CouchDbInstance dbInstance;
 	
-	public static Result current() {
+	public Result current() {
 		
 		return async(
-			    future(new Callable<BoatPosition>() {
-			      public BoatPosition call() {
-			    	position.move();
-					return position;
+			    future(new Callable<JsonNode>() {
+			      public JsonNode call() {
+			    	  
+					CouchDbConnector db = dbInstance.createConnector("positions", true);
+
+					ChangesCommand cmd = new ChangesCommand.Builder().includeDocs(true).build();
+
+					ChangesFeed feed = db.changesFeed(cmd);
+					try {
+						while (feed.isAlive()) {
+							DocumentChange change = feed.next();
+							return change.getDocAsNode();
+						}
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+			    	
+					return JsonNodeFactory.instance.textNode("error in fetching changes");
 			      }   
-			    }).map(new Function<BoatPosition, Result>() {
-					public Result apply(BoatPosition position){
-						return ok(Json.toJson(position));
+			    }).map(new Function<JsonNode, Result>() {
+					public Result apply(JsonNode position){
+						return ok(position);
 					}
 			    })
 			  );
